8cf6dbdf633a13f89616d6a41755ef30debf2672
Jiyaa22 <60835773+Jiyaa22@users.noreply.github.com>
2020-11-06 10:15:31 +0530

    Add files via upload

---
+++ database.js
@@ -0,0 +1,64 @@
+const dictionary = {
+  "hello" : {
+    "word" : "hello",
+    "lexicalCategory":"Noun",
+    "definition" : "an utterance of ‘hello’; a greeting.",
+    "sentence" : "she was getting polite nods and hellos from people"
+  },
+  "anonymous" : {
+    "word" : "anonymous",
+    "lexicalCategory":"Adjective",
+    "definition" :"(of a person) not identified by name; of unknown name.",
+    "sentence" :"the donor's wish to remain anonymous"
+  },
+  "externally" : {
+    "word" : "externally",
+    "lexicalCategory":"Adverb",
+    "definition" : "with reference to the outer surface or structure of something; outside",
+    "sentence" :"the antenna should be mounted externally, perhaps on the roof"
+  },
+  "grand" : {
+    "word" : "grand",
+    "lexicalCategory":"Adjective",
+    "definition" : "magnificent and imposing in appearance, size, or style.",
+    "sentence" :"a grand country house"
+  },
+  "incumbent" : {
+    "word" : "incumbent",
+    "lexicalCategory":"Adjective",
+    "definition" : "necessary for (someone) as a duty or responsibility.",
+    "sentence" :"the government realized that it was incumbent on them to act"
+  },
+  "disguise" : {
+    "word" : "disguise",
+    "lexicalCategory":"Verb",
+    "definition" : "give (someone or oneself) a different appearance in order to conceal one's identity.",
+    "sentence" :"we took elaborate measures to disguise ourselves as locals"
+  },
+  "she" : {
+    "word" : "she",
+    "lexicalCategory":"Pronoun",
+    "definition" : "used to refer to a woman, girl, or female animal previously mentioned or easily identified",
+    "sentence" :"my sister told me that she was not happy"
+  },
+  "star" : {
+    "word" : "star",
+    "lexicalCategory":"Noun",
+    "definition" : "a fixed luminous point in the night sky which is a large, remote incandescent body like the sun.",
+    "sentence" :"look at the beautiful stars in the sky!"
+  },
+  "gracious" : {
+    "word" : "gracious",
+    "lexicalCategory":"Adjective",
+    "definition" : "courteous, kind, and pleasant, especially towards someone of lower social status.",
+    "sentence" :"she is a gracious hostess"
+  },
+  "pardon" : {
+    "word" : "pardon",
+    "lexicalCategory":"Noun",
+    "definition" : "the action of forgiving or being forgiven for an error or offence.",
+    "sentence" :"he obtained pardon for his sins"
+  }
+}
+
+export default dictionary
